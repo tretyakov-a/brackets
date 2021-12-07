119e0659e12ed732c089be8868c2cf391b923115
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-07 11:00:21 +0300

    feat: implement required functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = new Map(bracketsConfig);
+  const closeBrackets = new Set(brackets.values());
+
+  const stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    const ch = str[i];
+    if (brackets.get(ch) === ch) {
+      if (stack[stack.length - 1] !== ch) {
+        stack.push(ch);
+      } else {
+        stack.pop(ch);
+      }
+    } else if (brackets.has(ch)) {
+      stack.push(ch);
+    } else if (closeBrackets.has(ch)) {
+      if (stack.length === 0) {
+        return false;
+      }
+      const top = stack.pop();
+      if (brackets.get(top) !== ch) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
